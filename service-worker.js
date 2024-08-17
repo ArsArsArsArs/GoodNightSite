363a8cf7e2e0ba77ae0b22a6afe3cd05beff9296
ArsArsArsArs <galochkaarseny@gmail.com>
2024-08-17 12:09:24 +0300

    moving service worker

--- service-worker.js
+++ service-worker.js
@@ -22,6 +22,8 @@ self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(urlsToCache);
+        }).catch(err => {
+            console.error(`Failed to cache: ${err}`);
         })
     );
 });
